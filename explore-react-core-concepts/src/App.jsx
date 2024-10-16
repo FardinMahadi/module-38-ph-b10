01e3a37eda864f1e570380ed61efd7592e5e034c
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-10-16 18:33:54 +0600

    props

---
+++ explore-react-core-concepts/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Device name="Laptop" price="55" />
+      <Device name="Mobile" price="32" />
+      <Device name="Tablet" price="98" />
+      <Person />
+      <Student grade="7" score="99" />
+      <Student grade="8" score="44" />
+      <Student grade={12} score="33" />
+      <Student />
+      <Developer />
+    </>
+  );
+}
+
+function Device({ name, price }) {
+  return (
+    <h2>
+      This device: {name} <br /> Price: {price}
+    </h2>
+  );
+}
+
+function Person() {
+  const age = 25;
+  const money = 20;
+  const person = { name: "Fardin", age: 20 };
+  return (
+    <h3>
+      I am {person.name} with age: {age + money}
+    </h3>
+  );
+}
+
+function Student({ grade = 1, score = 0 }) {
+  return (
+    <div className="student">
+      <h3>This is a student</h3>
+      <p>Class: {grade}</p>
+      <p>Score: {score}</p>
+    </div>
+  );
+}
+
+function Developer() {
+  const deveploperStyle = {
+    margin: "20px",
+    padding: "20px",
+    border: "2px solid purple",
+    borderRadius: "20px",
+  };
+
+  return (
+    <div style={deveploperStyle}>
+      <h5>Devo devo</h5>
+      <p>Coding: </p>
+    </div>
+  );
+}
+
+export default App;
